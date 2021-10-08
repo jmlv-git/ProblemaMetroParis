c83db98d8dc687fa2327c3f1636fe0cdbae35f29
Joao Marcos <55150938+jmlv-git@users.noreply.github.com>
2021-10-08 12:10:14 -0300

    Teste 1 - Ordenamento errado
    Possível falha nas matrizes

--- Main.java
+++ Main.java
@@ -1,261 +1,383 @@
 import java.util.*;
 
-
 public class Main {
-	
-	
-	
-	
-	
-	public static void adicionar(Node[] a, Node b) {
-		Node[] a2= new Node[a.length+1];
+
+	public static Node[] adicionar(Node[] a, Node b) {
+		Node[] a2 = new Node[a.length + 1];
 		for (int i = 0; i < a.length; i++) {
-			a2[i+1]=a[i];
+			a2[i + 1] = a[i];
 		}
-		a2[0]=b;
-		a=a2;
+		a2[0] = b;
+		return a = a2;
 	}
-	
-	public static void remover(Node[] a) {
-		Node[] a2= new Node[a.length-1];
+
+	public static Node[] remover(Node[] a) {
+		Node[] a2 = new Node[a.length - 1];
 		for (int i = 1; i < a.length; i++) {
-			a2[i-1]=a[i];
+			a2[i - 1] = a[i];
 		}
-		a=a2;
+		return a = a2;
 	}
-	
+
 	public static void append(int[] a, int size, int b) {
-		a[size]=b;
+		a[size] = b;
 	}
-	
-	public static void ordenar(Node[] a) {
-		
-		
-		Node aux ;
+
+	public static Node[] ordenar(Node[] a) {
+
+		Node aux;
 		int i = 0;
-		
-		
-		
-		for(i = 0; i<a.length; i++){
-			for(int j = 0; j<a.length-1; j++){
-				if(!a[j].equals(null) && !a[j+1].equals(null) && a[j].g+a[j].h > a[j + 1].g+a[j + 1].h){
-					aux = a[j];//verificar essa questao do null
-					a[j] = a[j+1];
-					a[j+1] = aux;
+
+		for (i = 0; i < a.length; i++) {
+			for (int j = 0; j < a.length - 1; j++) {
+				if (a[j]!=null && a[j + 1]!=null && a[j].g + a[j].h > a[j + 1].g + a[j + 1].h) {
+					aux = a[j];// verificar essa questao do null
+					a[j] = a[j + 1];
+					a[j + 1] = aux;
 				}
 			}
 		}
-		
-		
+		return a;
 	}
-	
-	
-	public static void main(String[] args) {
-		Scanner in =new Scanner(System.in);
-		float[][] h = new float[14][14];
 
-		for (int i = 0; i < h.length; i++) {
-			for (int j = 0; j < h.length; j++) {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		float[][] hAll = new float[14][14];
+
+		for (int i = 0; i < hAll.length; i++) {
+			for (int j = 0; j < hAll.length; j++) {
 
 				if (i == j) {
-					h[i][j] = 0;
+					hAll[i][j] = 0;
 				} else {
-					h[i][j] = -1;
+					hAll[i][j] = -1;
 				}
 			}
 		}
-		
-		
+
 		// Preenchendo tabela:
 		// linha 1
-		h[0][1] = 10;
-		h[0][2] = 18.5f;
-		h[0][3] = 24.8f;
-		h[0][4] = 36.4f;
-		h[0][5] = 38.8f;
-		h[0][6] = 35.8f;
-		h[0][7] = 25.4f;
-		h[0][8] = 17.6f;
-		h[0][9] = 9.1f;
-		h[0][10] = 16.7f;
-		h[0][11] = 27.3f;
-		h[0][12] = 27.6f;
-		h[0][13] = 29.8f;
+		hAll[0][1] = 10;
+		hAll[0][2] = 18.5f;
+		hAll[0][3] = 24.8f;
+		hAll[0][4] = 36.4f;
+		hAll[0][5] = 38.8f;
+		hAll[0][6] = 35.8f;
+		hAll[0][7] = 25.4f;
+		hAll[0][8] = 17.6f;
+		hAll[0][9] = 9.1f;
+		hAll[0][10] = 16.7f;
+		hAll[0][11] = 27.3f;
+		hAll[0][12] = 27.6f;
+		hAll[0][13] = 29.8f;
 		// linha 2
-		h[1][2] = 8.5f;
-		h[1][3] = 14.8f;
-		h[1][4] = 26.6f;
-		h[1][5] = 29.1f;
-		h[1][6] = 26.1f;
-		h[1][7] = 17.3f;
-		h[1][8] = 10f;
-		h[1][9] = 3.5f;
-		h[1][10] = 15.5f;
-		h[1][11] = 20.9f;
-		h[1][12] = 19.1f;
-		h[1][13] = 21.8f;
+		hAll[1][2] = 8.5f;
+		hAll[1][3] = 14.8f;
+		hAll[1][4] = 26.6f;
+		hAll[1][5] = 29.1f;
+		hAll[1][6] = 26.1f;
+		hAll[1][7] = 17.3f;
+		hAll[1][8] = 10f;
+		hAll[1][9] = 3.5f;
+		hAll[1][10] = 15.5f;
+		hAll[1][11] = 20.9f;
+		hAll[1][12] = 19.1f;
+		hAll[1][13] = 21.8f;
 		// linha 3
-		h[2][3] = 6.3f;
-		h[2][4] = 18.2f;
-		h[2][5] = 20.6f;
-		h[2][6] = 17.6f;
-		h[2][7] = 13.6f;
-		h[2][8] = 9.4f;
-		h[2][9] = 10.3f;
-		h[2][10] = 19.5f;
-		h[2][11] = 19.1f;
-		h[2][12] = 12.1f;
-		h[2][13] = 16.6f;
+		hAll[2][3] = 6.3f;
+		hAll[2][4] = 18.2f;
+		hAll[2][5] = 20.6f;
+		hAll[2][6] = 17.6f;
+		hAll[2][7] = 13.6f;
+		hAll[2][8] = 9.4f;
+		hAll[2][9] = 10.3f;
+		hAll[2][10] = 19.5f;
+		hAll[2][11] = 19.1f;
+		hAll[2][12] = 12.1f;
+		hAll[2][13] = 16.6f;
 		// linha 4
-		h[3][4] = 12f;
-		h[3][5] = 14.4f;
-		h[3][6] = 11.5f;
-		h[3][7] = 12.4f;
-		h[3][8] = 12.6f;
-		h[3][9] = 16.7f;
-		h[3][10] = 23.6f;
-		h[3][11] = 18.6f;
-		h[3][12] = 10.6f;
-		h[3][13] = 15.4f;
+		hAll[3][4] = 12f;
+		hAll[3][5] = 14.4f;
+		hAll[3][6] = 11.5f;
+		hAll[3][7] = 12.4f;
+		hAll[3][8] = 12.6f;
+		hAll[3][9] = 16.7f;
+		hAll[3][10] = 23.6f;
+		hAll[3][11] = 18.6f;
+		hAll[3][12] = 10.6f;
+		hAll[3][13] = 15.4f;
 		// linha 5
-		h[4][5] = 3f;
-		h[4][6] = 2.4f;
-		h[4][7] = 19.4f;
-		h[4][8] = 23.3f;
-		h[4][9] = 28.2f;
-		h[4][10] = 34.2f;
-		h[4][11] = 24.8f;
-		h[4][12] = 14.5f;
-		h[4][13] = 17.9f;
+		hAll[4][5] = 3f;
+		hAll[4][6] = 2.4f;
+		hAll[4][7] = 19.4f;
+		hAll[4][8] = 23.3f;
+		hAll[4][9] = 28.2f;
+		hAll[4][10] = 34.2f;
+		hAll[4][11] = 24.8f;
+		hAll[4][12] = 14.5f;
+		hAll[4][13] = 17.9f;
 		// linha 6
-		h[5][6] = 3.3f;
-		h[5][7] = 22.3f;
-		h[5][8] = 25.7f;
-		h[5][9] = 30.3f;
-		h[5][10] = 36.7f;
-		h[5][11] = 27.6f;
-		h[5][12] = 15.2f;
-		h[5][13] = 18.2f;
+		hAll[5][6] = 3.3f;
+		hAll[5][7] = 22.3f;
+		hAll[5][8] = 25.7f;
+		hAll[5][9] = 30.3f;
+		hAll[5][10] = 36.7f;
+		hAll[5][11] = 27.6f;
+		hAll[5][12] = 15.2f;
+		hAll[5][13] = 18.2f;
 		// linha 7
-		h[6][7] = 20f;
-		h[6][8] = 23f;
-		h[6][9] = 27.3f;
-		h[6][10] = 34.2f;
-		h[6][11] = 25.7f;
-		h[6][12] = 12.4f;
-		h[6][13] = 15.6f;
+		hAll[6][7] = 20f;
+		hAll[6][8] = 23f;
+		hAll[6][9] = 27.3f;
+		hAll[6][10] = 34.2f;
+		hAll[6][11] = 25.7f;
+		hAll[6][12] = 12.4f;
+		hAll[6][13] = 15.6f;
 		// linha 8
-		h[7][8] = 8.2f;
-		h[7][9] = 20.3f;
-		h[7][10] = 16.1f;
-		h[7][11] = 6.4f;
-		h[7][12] = 22.7f;
-		h[7][13] = 27.6f;
+		hAll[7][8] = 8.2f;
+		hAll[7][9] = 20.3f;
+		hAll[7][10] = 16.1f;
+		hAll[7][11] = 6.4f;
+		hAll[7][12] = 22.7f;
+		hAll[7][13] = 27.6f;
 		// linha 9
-		h[8][9] = 13.5f;
-		h[8][10] = 11.2f;
-		h[8][11] = 10.9f;
-		h[8][12] = 21.2f;
-		h[8][13] = 26.6f;
+		hAll[8][9] = 13.5f;
+		hAll[8][10] = 11.2f;
+		hAll[8][11] = 10.9f;
+		hAll[8][12] = 21.2f;
+		hAll[8][13] = 26.6f;
 		// linha 10
-		h[9][10] = 17.6f;
-		h[9][11] = 24.2f;
-		h[9][12] = 18.7f;
-		h[9][13] = 21.2f;
+		hAll[9][10] = 17.6f;
+		hAll[9][11] = 24.2f;
+		hAll[9][12] = 18.7f;
+		hAll[9][13] = 21.2f;
 		// linha 11
-		h[10][11] = 14.2f;
-		h[10][12] = 31.5f;
-		h[10][13] = 35.5f;
+		hAll[10][11] = 14.2f;
+		hAll[10][12] = 31.5f;
+		hAll[10][13] = 35.5f;
 		// linha 12
-		h[11][12] = 28.8f;
-		h[11][13] = 33.6f;
+		hAll[11][12] = 28.8f;
+		hAll[11][13] = 33.6f;
 		// linha 13
-		h[12][13] = 5.1f;
+		hAll[12][13] = 5.1f;
 
 		// espelhando valores adicionados
 
-		for (int i = 0; i < h.length; i++) {
-			for (int j = 0; j < h.length; j++) {
+		for (int i = 0; i < hAll.length; i++) {
+			for (int j = 0; j < hAll.length; j++) {
 
-				if (h[i][j] == -1) {
-					h[i][j] = h[j][i];
+				if (hAll[i][j] == -1) {
+					hAll[i][j] = hAll[j][i];
 				} // poderia colocar um else if para quando chegasse em uma celula
 					// diferente de -1 e 0, entao parava esse for (ia para a prox linha)
 			}
 		}
 
-		
 		// transformando tabela h de distancias ideais entre estações para gasto de
 		// tempo ideal (em minuto):
 
-		for (int i = 0; i < h.length; i++) {
-			for (int j = 0; j < h.length; j++) {
-				h[i][j] = (h[i][j] / 30) * 60;
+		for (int i = 0; i < hAll.length; i++) {
+			for (int j = 0; j < hAll.length; j++) {
+				hAll[i][j] = (hAll[i][j] / 30) * 60;
 			}
 		}
 
-		
+		// construindo a tabela de distancias reais
+		float[][] distanciaReal = new float[14][14];
+		// preenchendo toda tabela com zero. para garantir que sabemos tudo que tem lá
+		for (int i = 0; i < distanciaReal.length; i++) {
+			for (int j = 0; j < distanciaReal.length; j++) {
 
-		
-	System.out.println("Digite o número de sua estação atual");	
-		int Ei=in.nextInt();
-		Ei--;	//estacao x fica no indice x-1
-	System.out.println("Digite o número da estação final desejada");	
-		int Ef=in.nextInt();	
-		Ef--;
-		float[] funcH= h[Ef];//função heuristica que vai ser usada
-		float[][] g = new float[14][14];// dist real, matriz de adjacencias
-		Node[] front = new Node[1];
-		
-		ArrayList<Node> caminho= new ArrayList<>();
-		
-		Node Na= new Node(Ei, funcH[Ei],0);
-		Node Nf= new Node(Ef, 0,0);
-		
-		//adicionar(front,Na);
-		front[0]=Na;
-		while (true) {
+				if (i == j) {
+					//distanciaReal[i][j] = 0;// talvez tenhamos q colocar -1, pois nao exite aresta do nó x->x
+					distanciaReal[i][j] = -1;
+				} else {
+					distanciaReal[i][j] = -1;
+				}
+			}
+		}
+
+		distanciaReal[0][1] = 10;
+		distanciaReal[1][2] = 8.5f;
+		distanciaReal[1][8] = 10;
+		distanciaReal[1][9] = 3.5f;
+		distanciaReal[2][8] = 9.4f;
+		distanciaReal[2][12] = 18.7f;
+		distanciaReal[3][4] = 13;
+		distanciaReal[3][7] = 15.3f;
+		distanciaReal[3][12] = 12.8f;
+		distanciaReal[4][5] = 3;
+		distanciaReal[4][6] = 2.4f;
+		distanciaReal[4][7] = 30;
+		distanciaReal[7][8] = 9.6f;
+		distanciaReal[7][11] = 6.4f;
+		distanciaReal[8][10] = 12.2f;
+		distanciaReal[12][13] = 5.1f;
+		//espelhando valore ((x,y)=(y,x))
+		for (int i = 0; i < distanciaReal.length; i++) {
+			for (int j = 0; j < distanciaReal.length; j++) {
+
+				if (distanciaReal[i][j] == -1) {
+					distanciaReal[i][j] = distanciaReal[j][i];
+				}
+			}
+		}//transformando a dist em tempo
+		for (int i = 0; i < distanciaReal.length; i++) {
+			for (int j = 0; j < distanciaReal.length; j++) {
+				distanciaReal[i][j] = (distanciaReal[i][j] / 30) * 60;
+			}
+		}
+		//printando
+//		for (int i = 0; i < distanciaReal.length; i++) {
+//			for (int j = 0; j < distanciaReal.length; j++) {
+//				System.out.print(distanciaReal[i][j] + "   	");
+//			}
+//			System.out.println();
+//		}
+
+	
+
+	
+	Edge[][] g = new Edge[14][14] ;// dist real, matriz de adjacencias (fiz isso pois nome E muito grande)
+	// tranformando cada celula da tabela de adjacencias em objetos do tipo Edge
+	for (int i = 0; i < g.length; i++) {
+		for (int j = 0; j < g.length; j++) {
+			g[i][j]=new Edge(i, j, distanciaReal[i][j], null);
+		}
+	}
+	//identificando linhas
+	//E1
+	g[0][1].linha="AZ";
+	//E2
+	g[1][2].linha="AZ";
+	g[1][8].linha="AM";
+	g[1][9].linha="AM";
+	//E3
+	g[2][3].linha="AZ";
+	g[2][8].linha="VM";
+	g[2][12].linha="VM";
+	//E4
+	g[3][4].linha="AZ";
+	g[3][7].linha="VD";
+	g[3][12].linha="VD";
+	//E5
+	g[4][5].linha="AZ";
+	g[4][6].linha="AM";
+	g[4][7].linha="AM";
+	//E6 e E7 nao precisa
+	//E8
+	g[7][8].linha="AM";
+	g[7][11].linha="VD";
+	//E9
+	g[8][10].linha="VM";
+	//E13
+	g[12][13].linha="VD";
+	//espelhando valores de linha
+	for (int i = 0; i < g.length; i++) {
+		for (int j = 0; j < g.length; j++) {
+			if (g[i][j].linha!=null) {// verificar se o correto é .equals ou ==
+				g[j][i].linha=g[i][j].linha;
+			}
+		}
+	}
+	
+	
+	System.out.println("Digite o número de sua estação atual");
+
+	int Ei=in.nextInt();Ei--; // estacao x fica no indice x-1
+	System.out.println("Digite o número da estação final desejada");
+	int Ef = in.nextInt();Ef--;
+	float[] h = hAll[Ef];// função heuristica que vai ser usada
+	
+	Node[] front = new Node[0];
+
+	
+
+	
+
+	boolean foi =false;
+	if (Ei==Ef) {
+		foi = true;
+	}else {
+		for (int i = 0; i < g.length; i++) {
+			if (g[Ei][i].peso>0) {
+				Node newNode= new Node(i, h[i], g[Ei][i].peso, g[Ei][i].linha);
+				newNode.path.add(Ei);
+				front=adicionar(front, newNode);
+			}
+		}
+		front=ordenar(front);
+		for (int i = 0; i < front.length; i++) {
+			System.out.print(front[i].id+1+" ");
+		}System.out.println();
+	}
+	Node Na;
+	while(!foi){
+		Na=front[0];
+		front=remover(front);
+		if (Na.id==Ef) {
+			foi=true;
+			Na.path.add(Ef);
+			System.out.println("Caminho:");
+			for (int i = 0; i < Na.path.size(); i++) {
+				System.out.print(Na.path.get(i)+1+" ");
+			}System.out.println();
+			System.out.println("Tempo:" + Na.g);
+		}else {
 			
-			if (Na.id==Nf.id) {
-				break;
-			}else {
-				remover(front);
-				caminho.add(Na);
-				for (int i = 0; i < g.length; i++) {
-					if (g[Na.id][i]!=-1) {
-						Node newNode= new Node(i, funcH[i],g[Na.id][i]+Na.g);//id=i h = funcH g = dist do no atual + dist acumulada
-						adicionar(front,newNode);
+			for (int i = 0; i < g.length; i++) {
+				if (g[Na.id][i].peso>0) {
+					Node newNode= new Node(i, h[i], g[Na.id][i].peso + Na.g, g[Na.id][i].linha);
+					for (int j=0; j < Na.path.size(); j++) {
+						newNode.path.add(Na.path.get(j));// adicionando todos do caminho de Na
 					}
+					newNode.path.add(Na.id);// adicionando Na no caminho de newNode
+					if(!Na.linhaUsada.equals(newNode.linhaUsada)){// verificacao de baldeacao, se linha usada para chegar em Na sera a mesma para ir de Na para newNode
+					newNode.linhaUsada=newNode.linhaUsada+4;
+					}	
+					front=adicionar(front, newNode);
 				}
-				//ordenar a fronteira
-				ordenar(front);
-				for (int i = 0; i < front.length; i++) {
-					if(front[i].equals(null)) {
-						System.out.println("null  ");
-					}else
-					System.out.print(front[i]+"  ");
-				}
-				Na=front[0];
 			}
-			
+			front=ordenar(front);
+			for (int i = 0; i < front.length; i++) {
+				System.out.print(front[i].id+1+" ");
+			}System.out.println();
 		}
-		
-		
-		
 	}
 
 }
 
-class Node{
+}
+
+class Node {
 	int id;
 	float f;
 	float g;
 	float h;
-	public Node(int nDoNode,float h,float g) {
-		id=nDoNode;
-		this.g=g;
-		this.h=h;
-		this.f=h;// nao está sendo util
+	String linhaUsada;
+	ArrayList<Integer> path;
+
+	public Node(int nDoNode, float h, float g,String linhaUsada) {
+		id = nDoNode;
+		this.g = g;
+		this.h = h;
+		this.f = h;// nao está sendo util
+		this.linhaUsada=linhaUsada;
+		path= new ArrayList<Integer>();
 	}
 }
 
+class Edge{
+	int de;
+	int para;
+	float peso;
+	String linha;
+	
+	public Edge (int de, int para, float peso, String linha) {
+		this.de=de;
+		this.para=para;
+		this.peso=peso;
+		this.linha=linha;
+	}
+	
+}
